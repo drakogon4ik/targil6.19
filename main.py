995b21b783f88016682a52f267d8d56fa299b3be
drakogon4ik <94756197+drakogon4ik@users.noreply.github.com>
2024-03-02 19:27:26 +0200

    adding send_recv function

--- main.py
+++ main.py
@@ -22,6 +22,18 @@ def checking_ack_packet(syn_ack_packet):
     return syn_ack_packet.haslayer(TCP) and syn_ack_packet[TCP].flags == 0x12
 
 
+def send_recv(destination_ip, destination_port):
+    """
+    function sends SYN packet and gets answer from the server
+    :param destination_ip: ip on which function sends syn packet
+    :param destination_port: port on which function sends syn packet
+    :return: answer from the server
+    """
+    syn_packet = IP(dst=destination_ip) / TCP(dport=destination_port, flags="S")
+
+    return sr1(syn_packet, timeout=0.5, verbose=False)
+
+
 def main(destination_ip):
     """
     loop that checks ports from 20 to 1024
@@ -31,13 +43,12 @@ def main(destination_ip):
     destination_port = 20
     try:
         while destination_port <= 1024:
-            syn_packet = IP(dst=destination_ip) / TCP(dport=destination_port, flags="S")
 
-            syn_ack_packet = sr1(syn_packet, timeout=0.5, verbose=False)
+            syn_ack_packet = send_recv(destination_ip, destination_port)
 
             if syn_ack_packet:
 
-                if checking_ack_packet(syn_ack_packet):  # 0x12 = SYN-ACK
+                if checking_ack_packet(syn_ack_packet):
                     print(f'SYN packet sent successfully. Received SYN-ACK packet on the port {destination_port}')
                     getting_ports.append(destination_port)
 
@@ -55,9 +66,8 @@ if __name__ == "__main__":
     """
     sending SYN packet on the closed port to check that function works
     """
-    packet = IP(dst=test_ip) / TCP(dport=test_port, flags="S")
-    res_packet = sr1(packet, verbose=False)
-    assert not checking_ack_packet(res_packet)
+    packet = send_recv(test_ip, test_port)
+    assert not checking_ack_packet(packet)
 
     ip = input('Please input ip you want to check: ')
     main(ip)
